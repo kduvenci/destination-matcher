6808c84685b2849973da049cfe79cb60e9e27136
Momoko Toyota <toyopet-momoko@hotmail.co.jp>
2018-11-19 18:58:50 +0900

    done with models

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,5 +1,6 @@
 class City < ApplicationRecord
   belongs_to :country
   has_many :accommodations
+  has_many :flights
   validates :name, :photo, :meal_average_price_cents
 end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,5 @@
 class Flight < ApplicationRecord
+  belongs_to :city
   has_many :favorites
   validates :depart_depature_time, :depart_arrival_time, :return_departure_time, :return_arrival_time, :departure_location, :return_location, :price, :airline_name
 end
