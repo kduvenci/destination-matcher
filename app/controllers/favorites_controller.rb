9f67f547257f4270b37eb917092141fec2742d85
Momoko Toyota <toyopet-momoko@hotmail.co.jp>
2018-11-20 21:53:04 +0900

    favorites index method created. index pages created(not yet finished

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,9 +1,19 @@
 class FavoritesController < ApplicationController
   def create
-    #code
-    #....
+    @favorite = Favorite.new
+    @user = current_user
+    @flight = Flight.find(params[:id])
+    @accomodation = Accommodation.find(params[:id])
+
+    @favorite.user = @user
+    @favorite.flight = @flight
+    @favorite.accommodation = @accomodation
+
     authorize(@favorite)
+
+    @favorite.save!
   end
+
   def index
     @favorites = policy_scope(Favorite).order(created_at: :desc)
   end
