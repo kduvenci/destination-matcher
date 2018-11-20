05846124fd4008ef1f19b18fea8c6bec89a125e2
Amanda Dilworth <amandadilworth@yahoo.com>
2018-11-20 16:09:43 +0900

    added search function to find flights

--- app/services/search_flights.rb
+++ app/services/search_flights.rb
@@ -5,8 +5,23 @@ class SearchFlights
   # output:
   # [flight1, flight2...]
   def self.call(params)
-    flights = []
-    params[:region]
-    flights
+    flights = Flight.all
+    flights = flights.where(departure_location: params[:origin]) if params[:origin]
+    flights = flights.where(return_location: params[:region]) if params[:region]
+    flights = flights.where("price >= ?", params[:min_budget]) if params[:min_budget]
+    flights = flights.where("price <= ?", params[:max_budget]) if  params[:max_budget]
+    flights = flights.where(depart_departure_time: params[:dep_date]) if params[:dep_date]
+    flights = flights.where(return_departure_time: params[:return_date]) if params[:return_date]
+    return flights
   end
 end
+
+    # depart_departure_time: Time.now,
+    # depart_arrival_time: Time.now,
+    # return_departure_time: Time.now,
+    # return_arrival_time: Time.now,
+    # departure_location: city_names[counter],
+    # return_location: city_names[counter],
+    # price: 1000,
+    # city: cities[counter],
+    # airline_name: airline_names[counter])
