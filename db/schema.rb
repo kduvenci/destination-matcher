33d19817ca5c821e4204912ee447305d9051f55c
Momoko Toyota <toyopet-momoko@hotmail.co.jp>
2018-11-22 19:09:02 +0900

    added address column to accommodations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_22_084022) do
+ActiveRecord::Schema.define(version: 2018_11_22_100234) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2018_11_22_084022) do
     t.string "photo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "address"
     t.index ["city_id"], name: "index_accommodations_on_city_id"
   end
 
