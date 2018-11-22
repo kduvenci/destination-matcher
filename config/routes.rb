b5fc82502f19329d35a834a79833efd55f7600b5
Momoko Toyota <toyopet-momoko@hotmail.co.jp>
2018-11-22 10:11:42 +0900

    fixed navbar. also changed routes to allow user to access his/her favorites page.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'cities#index'
   resources :cities, only: [:index, :show] do
-    resources :favorites, only: [:index, :create]
+    resources :favorites, only: [:create]
   end
+
+  resources :favorites, only: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
