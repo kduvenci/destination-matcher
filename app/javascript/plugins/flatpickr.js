9f28c436a1d5f1af0ae7ff4845063b39fcff3fcf
Momoko Toyota <toyopet-momoko@hotmail.co.jp>
2018-11-28 15:40:53 +0900

    set default values to search form and changed stylings

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -2,10 +2,14 @@ import flatpickr from "flatpickr"
 import "flatpickr/dist/flatpickr.min.css" // Note this is important!
 import rangePlugin from "flatpickr/dist/plugins/rangePlugin"
 
-flatpickr(".datepicker", {
+flatpickr(".depart-date", {
   minDate: "today",
-  defaultDate: ["today"]
-  // maxDate: new Date().fp_incr(7)
+  defaultDate:  new Date().fp_incr(1),
+})
+
+flatpickr(".return-date", {
+  minDate: "today",
+  defaultDate: new Date().fp_incr(8)
 })
 
 flatpickr("#range_start", {
