20334be0b7b762e1b94c04f56a0bebf5f5cb5d31
Ricardo Piras <ricpiras@gmail.com>
2018-11-28 18:31:52 +0900

    Added working form: parsed seeds, changed controle and policy

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
   resources :favorites, only: [:index, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :visas, only: [:show]
+  get "/visas/search", to: "visas#search", as: :search
 end
