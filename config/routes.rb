27a86071bdf97402d688fce515494928c5573125
Ricardo Piras <ricpiras@gmail.com>
2018-11-28 15:59:43 +0900

    Created visas controller

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
 
   resources :favorites, only: [:index, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :visas, only: [:show]
 end
