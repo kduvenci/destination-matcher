97b2949758da66cd02d01e65b163f6d297a84377
kemal duvenci <k.duvenci@gmail.com>
2018-11-29 19:53:36 +0900

    jsSearchForm uncommented

--- app/javascript/packs/search_form.js
+++ app/javascript/packs/search_form.js
@@ -1,8 +1,8 @@
-// select date for next date picker
-const departureDate = document.getElementById("_cities_dep_date");
-const returnDate = document.getElementById("_cities_return_date");
+// // select date for next date picker
+// const departureDate = document.getElementById("_cities_dep_date");
+// const returnDate = document.getElementById("_cities_return_date");
 
-departureDate.addEventListener('input', () => {
-  returnDate.value = departureDate.value;
-  // window.jQuery('#_cities_return_date').datepicker().trigger('setDate', new Date(1999, 1, 1));
-});
+// departureDate.addEventListener('input', () => {
+//   returnDate.value = departureDate.value;
+//   // window.jQuery('#_cities_return_date').datepicker().trigger('setDate', new Date(1999, 1, 1));
+// });
